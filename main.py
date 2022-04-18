fb41274be8eb5fba449389e14f78dfdd15058c77
annabelxu <annabel.xu@chicagobooth.edu>
2022-04-18 16:53:16 -0500

    1st
    1st

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from google.cloud import storage
+
+app = Flask(__name__)
+CLOUD_STORAGE_BUCKET = os.environment.get('CLOUD_STORAGE_BUCKET')
+
+
+@app.route("/")
+def homepage():
+    return "Hello Photo Timeline!"
+
+@app.route("/upload", methods=['POST'])
+
+def upload():
+    photo = request.files['file']
+    print(photo.filename)
+    storage_client = storage.Client.from_service_account_json('photo-timeline-shared-347519-a62ca851fc20.json')
+    bucket_name = CLOUD_STORAGE_BUCKET
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(photo.filename)
+    blob.upload_from_string(photo.read(), content_type=photo.content_type)
+    print(f"File uploaded: {photo.filename} to {blob.public_url}")
+    return ":)"
+
+if __name__ == '__main__':
+  app.run(debug=True)
